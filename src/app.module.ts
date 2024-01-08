865eee0fea46d6860165e6fd0e1c12f436ccb271
mnlrpr <mnlrpr@gmail.com>
2024-01-08 14:03:22 -0500

    Config enviroment

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizModule } from './modules/quiz/quiz.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 import {TypeOrmModule} from '@nestjs/typeorm'
 import { Quiz } from './modules/quiz/quiz.entity';
 import { Test } from './modules/test/test.entity';
@@ -14,16 +15,34 @@ import { Users } from './modules/users/users.entity';
 import { AnswersModule } from './modules/answers/answers.module';
 import { Answers } from './modules/answers/answers.entity';
 @Module({
-  imports: [QuizModule, DatabaseModule, TypeOrmModule.forRoot({
+  imports: [
+    ConfigModule.forRoot(),
+    QuizModule, 
+    DatabaseModule, 
+  //   TypeOrmModule.forRoot({
+  //   type: 'mysql',
+  //   host: 'localhost',
+  //   port: 3306,
+  //   username: 'user',
+  //   password: 'secret',
+  //   database: 'QUIZ',
+  //   entities: [Quiz,Test,Questions,Users,Answers],
+  //   synchronize: true,
+  // }),
+  TypeOrmModule.forRoot({
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'user',
-    password: 'secret',
-    database: 'QUIZ',
-    entities: [Quiz,Test,Questions,Users,Answers],
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT, 10) || 3306,
+    username: process.env.DB_USERNAME || 'user',
+    password: process.env.DB_PASSWORD || 'secret',
+    database: process.env.DB_DATABASE || 'QUIZ',
+    entities: [Quiz, Test, Questions, Users, Answers],
     synchronize: true,
-  }), TestModule, QuestionsModule, UsersModule, AnswersModule],
+  }),  
+  TestModule, 
+  QuestionsModule, 
+  UsersModule, 
+  AnswersModule],
   controllers: [AppController],
   providers: [AppService],
 })
